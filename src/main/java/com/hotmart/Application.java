36fcaeb0ce399cc730d1bdd48f77cc9ff5748862
kleytonsilvano <kleyton.silvano@gmail.com>
2020-11-15 22:49:12 -0300

    rename application class

--- src/main/java/com/hotmart/Application.java
+++ src/main/java/com/hotmart/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioHotmartJobApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioHotmartJobApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
